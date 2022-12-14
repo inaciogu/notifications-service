4019f1b4a6065b026c1116114b4fac72878fe543
gustavo <gus.inacio@hotmail.com>
2022-12-13 21:45:49 -0300

    feat: send notification use case with test

---
+++ src/app/use-cases/send-notification.spec.ts
@@ -0,0 +1,18 @@
+import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications.repository';
+import { SendNotification } from './send-notification';
+
+describe('Send notification', () => {
+  it('Should be able to create a new notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const sendNotification = new SendNotification(notificationsRepository);
+
+    const { notification } = await sendNotification.execute({
+      category: 'social',
+      content: 'nova notificação',
+      recipientId: 'testid',
+    });
+
+    expect(notificationsRepository.notifications).toHaveLength(1);
+    expect(notification).toEqual(notificationsRepository.notifications[0]);
+  });
+});
