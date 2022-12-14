02cd796d63042958abde5d248533eff407749180
gustavo <gus.inacio@hotmail.com>
2022-12-13 21:49:46 -0300

    feat: infra database layer with prisma implementation

---
+++ src/infra/database/prisma/repositories/prisma-notifications.repository.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { Notification } from '../../../../app/entities/notification';
+import { NotificationsRepository } from '../../../../app/repositories/notifications.repository';
+import { PrismaService } from '../prisma.service';
+
+@Injectable()
+export class PrismaNotificationsRepository implements NotificationsRepository {
+  constructor(private prismaService: PrismaService) {}
+
+  async create(notification: Notification): Promise<void> {
+    const { id, category, content, recipientId, readAt, createdAt } =
+      notification;
+    await this.prismaService.notification.create({
+      data: {
+        id,
+        category,
+        content: content.value,
+        recipientId,
+        readAt,
+        createdAt,
+      },
+    });
+  }
+}
