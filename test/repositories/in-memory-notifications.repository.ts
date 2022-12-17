b6e65ca82e604887c0bf1dcbdf5ab1d582405912
gustavo <gus.inacio@hotmail.com>
2022-12-17 09:36:55 -0300

    test: implements methods on in memory repo

--- test/repositories/in-memory-notifications.repository.ts
+++ test/repositories/in-memory-notifications.repository.ts
@@ -1,10 +1,29 @@
-import { Notification } from '../../src/app/entities/notification';
-import { NotificationsRepository } from '../../src/app/repositories/notifications.repository';
+import { Notification } from '@app/entities/notification';
+import { NotificationsRepository } from '@app/repositories/notifications.repository';
+import { NotificationNotFound } from '@app/use-cases/errors/notification-not-found.error';
 
 export class InMemoryNotificationsRepository
   implements NotificationsRepository
 {
   public notifications: Notification[] = [];
+
+  async findById(notificationId: string): Promise<Notification | null> {
+    const notification = this.notifications.find(
+      (notification) => notification.id === notificationId,
+    );
+
+    return notification ?? null;
+  }
+
+  async save(notification: Notification): Promise<void> {
+    const notificationIndex = this.notifications.findIndex(
+      ({ id }) => notification.id === id,
+    );
+
+    if (notificationIndex < 0) throw new NotificationNotFound();
+
+    this.notifications[notificationIndex] = notification;
+  }
 
   async create(notification: Notification): Promise<void> {
     this.notifications.push(notification);
