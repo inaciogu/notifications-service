8540e97ecafdc3bf8f9e7a4f4ca5c1314cfb4e1a
gustavo <gus.inacio@hotmail.com>
2022-12-17 11:11:29 -0300

    feat: prisma repository methods implementation

--- src/infra/database/prisma/repositories/prisma-notifications.repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications.repository.ts
@@ -3,14 +3,49 @@ import { Notification } from '@app/entities/notification';
 import { NotificationsRepository } from '@app/repositories/notifications.repository';
 import { PrismaService } from '../prisma.service';
 import { PrismaNotificationMapper } from '../mappers/prisma-notification.mapper';
+import { Content } from '@app/entities/content';
 
 @Injectable()
 export class PrismaNotificationsRepository implements NotificationsRepository {
-  constructor(private prismaService: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
+
+  async findById(notificationId: string): Promise<Notification | null> {
+    const notification = await this.prisma.notification.findUnique({
+      where: { id: notificationId },
+    });
+
+    if (!notification) return null;
+
+    return PrismaNotificationMapper.toDomain(notification);
+  }
+
+  async findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+    const recipientNotifications = await this.prisma.notification.findMany({
+      where: { recipientId },
+    });
+    return recipientNotifications.map(PrismaNotificationMapper.toDomain);
+  }
+
+  async countManyByRecipientId(recipientId: string): Promise<number> {
+    const count = await this.prisma.notification.count({
+      where: { recipientId },
+    });
+
+    return count;
+  }
 
   async create(notification: Notification): Promise<void> {
     const raw = PrismaNotificationMapper.toPrisma(notification);
-    await this.prismaService.notification.create({
+    await this.prisma.notification.create({
+      data: raw,
+    });
+  }
+
+  async save(notification: Notification): Promise<void> {
+    const raw = PrismaNotificationMapper.toPrisma(notification);
+
+    await this.prisma.notification.update({
+      where: { id: notification.id },
       data: raw,
     });
   }
