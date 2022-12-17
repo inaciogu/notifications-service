c8122d91373a98e6fca4c4adeb2f8cc67ed97f88
gustavo <gus.inacio@hotmail.com>
2022-12-17 11:11:08 -0300

    feat: to domain mapper method

--- src/infra/database/prisma/mappers/prisma-notification.mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification.mapper.ts
@@ -1,4 +1,6 @@
+import { Notification as RawNotification } from '@prisma/client';
 import { Notification } from '@app/entities/notification';
+import { Content } from '@app/entities/content';
 
 export class PrismaNotificationMapper {
   static toPrisma(notification: Notification) {
@@ -12,5 +14,19 @@ export class PrismaNotificationMapper {
       readAt,
       createdAt,
     };
+  }
+
+  static toDomain(raw: RawNotification) {
+    return new Notification(
+      {
+        category: raw.category,
+        content: new Content(raw.content),
+        recipientId: raw.recipientId,
+        readAt: raw.readAt,
+        canceledAt: raw.canceledAt,
+        createdAt: raw.createdAt,
+      },
+      raw.id,
+    );
   }
 }
