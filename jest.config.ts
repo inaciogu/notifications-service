407db5a22af5590f20983dd4179c3d0ca6896716
gustavo <gus.inacio@hotmail.com>
2022-12-17 12:50:20 -0300

    test: jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,19 @@
-export default {
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { Config } from 'jest';
+import { compilerOptions } from './tsconfig.json';
+
+const config: Config = {
   moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: 'src',
   testRegex: '.*\\.spec\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
   collectCoverageFrom: ['**/*.(t|j)s'],
   coverageDirectory: '../coverage',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
   testEnvironment: 'node',
 };
+
+export default config;
