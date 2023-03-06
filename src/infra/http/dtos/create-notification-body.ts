2220ef47598240c600162f9a7e95d9458f1d35bb
gustavo <gus.inacio@hotmail.com>
2023-03-06 09:19:21 -0300

    feat (swagger): add dto properties to swagger

--- src/infra/http/dtos/create-notification-body.ts
+++ src/infra/http/dtos/create-notification-body.ts
@@ -1,13 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsUUID, Length } from 'class-validator';
 
 export class CreateNotificationBody {
+  @ApiProperty({ description: 'UUID' })
   @IsNotEmpty()
   @IsUUID()
   recipientId: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   category: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @Length(5, 240)
   content: string;
